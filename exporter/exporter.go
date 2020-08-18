c81562bb263fe3887720c41587f71e3667c3568a
Peter Pratscher <1674920+peterbitfly@users.noreply.github.com>
2020-08-18 20:29:11 +0200

    Show epoch data on index page during medalla finality issues

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -183,7 +183,7 @@ func Start(client rpc.Client) error {
 			if err != nil {
 				logger.Fatal(err)
 			}
-			startEpoch := head.HeadEpoch
+			startEpoch := head.HeadEpoch - 1
 			logger.Infof("exporting epoch data for epochs %v - %v", startEpoch, head.HeadEpoch)
 			err = updateEpochStatus(client, startEpoch, head.HeadEpoch)
 			if err != nil {
