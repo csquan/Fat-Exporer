552477f2ba2af7d868d71edcf56b62ff4f38becd
peter <peter@bitfly.at>
2020-12-08 09:51:44 +0100

    Update sql template

--- tables.sql
+++ tables.sql
@@ -7,21 +7,22 @@ In the future it is better to replace this table with an in memory cache (redis)
 drop table if exists validators;
 create table validators
 (
-    validatorindex             int    not null,
-    pubkey                     bytea  not null,
-    withdrawableepoch          bigint not null,
-    withdrawalcredentials      bytea  not null,
-    balance                    bigint not null,
-    effectivebalance           bigint not null,
-    slashed                    bool   not null,
-    activationeligibilityepoch bigint not null,
-    activationepoch            bigint not null,
-    exitepoch                  bigint not null,
+    validatorindex             int         not null,
+    pubkey                     bytea       not null,
+    withdrawableepoch          bigint      not null,
+    withdrawalcredentials      bytea       not null,
+    balance                    bigint      not null,
+    effectivebalance           bigint      not null,
+    slashed                    bool        not null,
+    activationeligibilityepoch bigint      not null,
+    activationepoch            bigint      not null,
+    exitepoch                  bigint      not null,
     lastattestationslot        bigint,
+    status                     varchar(20) not null default '',
     primary key (validatorindex)
 );
 create index idx_validators_pubkey on validators (pubkey);
-create index idx_validators_name on validators (name);
+create index idx_validators_status on validators (status);
 
 drop table if exists validator_names;
 create table validator_names
@@ -325,51 +326,51 @@ create table users
 drop table if exists oauth_apps;
 create table oauth_apps
 (
-    id                    serial                      not null,
-    owner_id              int                         not null,
-    redirect_uri          character varying(100)      not null unique,
-    app_name              character varying(35)       not null,
-    active                bool                        not null default 't',
-    created_ts            timestamp without time zone not null,
+    id           serial                      not null,
+    owner_id     int                         not null,
+    redirect_uri character varying(100)      not null unique,
+    app_name     character varying(35)       not null,
+    active       bool                        not null default 't',
+    created_ts   timestamp without time zone not null,
     primary key (id, redirect_uri)
 );
 
 drop table if exists oauth_codes;
 create table oauth_codes
 (
-    id              serial                      not null,
-    user_id         int                         not null,
-    code            character varying(64)       not null,
-    consumed        bool                        not null default 'f',
-    app_id          int                         not null,
-    created_ts      timestamp without time zone not null,
+    id         serial                      not null,
+    user_id    int                         not null,
+    code       character varying(64)       not null,
+    consumed   bool                        not null default 'f',
+    app_id     int                         not null,
+    created_ts timestamp without time zone not null,
     primary key (user_id, code)
 );
 
 drop table if exists users_devices;
 create table users_devices
 (
-    id                    serial                      not null,
-    user_id               int                         not null,
-    refresh_token         character varying(64)       not null,
-    device_name           character varying(20)       not null,
-    notification_token    character varying(500),
-    notify_enabled        bool                        not null default 't',
-    active                bool                        not null default 't',
-    app_id                int                         not null,
-    created_ts            timestamp without time zone not null,
+    id                 serial                      not null,
+    user_id            int                         not null,
+    refresh_token      character varying(64)       not null,
+    device_name        character varying(20)       not null,
+    notification_token character varying(500),
+    notify_enabled     bool                        not null default 't',
+    active             bool                        not null default 't',
+    app_id             int                         not null,
+    created_ts         timestamp without time zone not null,
     primary key (user_id, refresh_token)
 );
 
 drop table if exists users_clients;
 create table users_clients
 (
-    id                    serial                      not null,
-    user_id               int                         not null,
-    client         	      character varying(12)       not null,
-    client_version        int       			      not null,
-    notify_enabled        bool                        not null default 't',
-    created_ts            timestamp without time zone not null,
+    id             serial                      not null,
+    user_id        int                         not null,
+    client         character varying(12)       not null,
+    client_version int                         not null,
+    notify_enabled bool                        not null default 't',
+    created_ts     timestamp without time zone not null,
     primary key (user_id, client)
 );
 
